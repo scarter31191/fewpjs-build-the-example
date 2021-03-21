fdb339251a36085c1cac1cda090b6286b5b9996d
scarter31191 <scarter31191@gmaile.com>
2021-03-21 12:38:29 -0400

    Done.

--- main.js
+++ main.js
@@ -3,10 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector('#modal')
+// selects the <div id="modal"> #modal is just css selector for id="modal"
 
+function hiddenError() {
+  errorModal.classList.add("hidden")
+} // adds a class of "hidden" to the errorModal constant
 
+hiddenError() // calls function
 
+const articleHart = document.querySelectorAll('.like-glyph') //takes all the class="like-glyph" and makes a NodeList
+function likeCallBack(e) {
+  let heart = e.target 
+  mimicServerCall()
+  .then(function(serverMessage) {
+    if (heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART
+      heart.classList.add('activate-heart')
+    } else {
+      heart.innerText = EMPTY_HEART
+          heart.classList.remove("activated-heart")
+    }
+  })
+  .catch(function(error) {
+    const modal = document.getElementById('modal')
+    modal.className = "";
+    modal.innerText = error;
+    setTimeout(() => modal.className = "hidden", 3000);
+  });
+};
 
+for (const glyph of articleHart) {
+  addEventListener("click", likeCallBack);
+}
+ 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
